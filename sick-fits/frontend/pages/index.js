986469865da27c07e48f991d23ef8582c57e46cd
Sasha15 <alex_brovko@mail.ru>
2019-02-17 22:02:53 +0200

    add custom app, Link and Meta components

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,12 +1,7 @@
 //Main page 
-import Link from 'next/link';
-
 const Home = props => (
     <div>
         <p>Hey</p>
-        <Link href="/sell">
-            <a>Sell</a>
-        </Link>
     </div> 
 )
 
