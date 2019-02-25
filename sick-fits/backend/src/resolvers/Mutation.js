c2a2906b89fada15f385297a34f5de40463ea4b2
Sasha15 <alex_brovko@mail.ru>
2019-02-25 15:24:10 +0200

    update user signup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         //TODO check if they are logged in
@@ -28,6 +31,28 @@ const Mutations = {
         //2. check if they own that item, or have the permissions
         //3. delete it
         return ctx.db.mutation.deleteItem({ where }, info)
+    },
+    async signup(parent, args, ctx, info) {
+        //lowercase their email
+        args.email = args.email.toLowerCase()
+        //hash their password
+        const password = await bcrypt.hash(args.password, 10)
+        //create user in the database
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: {set: ['USER']}
+            }
+        }, info)
+        // create the JWT token for them
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+        //we set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365 //1 year cookie
+        })
+        return user
     }
 };
 
